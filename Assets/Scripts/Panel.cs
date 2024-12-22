511c29e7aa3906ab1d47466a26623d4f54fd5cac
OlegPylypchuk2004 <154813576+OlegPylypchuk2004@users.noreply.github.com>
2024-12-22 18:22:54 +0200

    Added fade preview

--- Assets/Scripts/Panel.cs
+++ Assets/Scripts/Panel.cs
@@ -1,14 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Panel : MonoBehaviour
 {
+    [SerializeField] private Image _fadePanel;
+
     public void Appear()
     {
         gameObject.SetActive(true);
+        _fadePanel.gameObject.SetActive(true);
     }
 
     public void Disappear()
     {
         gameObject.SetActive(false);
+        _fadePanel.gameObject.SetActive(false);
     }
 }
